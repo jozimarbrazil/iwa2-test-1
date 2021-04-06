c81e8046ee3da072874983e1b76c8e67f8fef32d
mikhail-cct <tayemnyj+cct@gmail.com>
2021-04-06 10:24:29 +0000

    wk8 final

--- routes.js
+++ routes.js
@@ -21,4 +21,7 @@ var multer = require("multer");
 var upload = multer({ dest: module.exports.UPLOAD_PATH});
 var imageCtrl = require('./image-controller');
 
+router.post('/images', upload.single('image'), imageCtrl.uploadImage);
+router.get('/images', imageCtrl.getImages);
+
 module.exports = router;
