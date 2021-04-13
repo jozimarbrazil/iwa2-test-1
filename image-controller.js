4bcd55875d43c310578c4cc8897a36a800478bf7
mikhail-cct <tayemnyj+cct@gmail.com>
2021-04-13 09:22:21 +0000

    wk9 changes

--- image-controller.js
+++ image-controller.js
@@ -4,19 +4,17 @@ path = require('path'),
 fs = require('fs'),
 del = require('del');
 
-exports.uploadImage = function(req, res)
-{
+exports.uploadImage = function(req, res) {
     let newImage = new Image();
     newImage.filename = req.file.filename;
     newImage.originalName = req.file.originalname;
     newImage.desc = req.body.desc;
     newImage.save(err => {
-        if (err){
+        if (err) {
             return res.sendStatus(400);
         }
         res.status(201).send({ newImage })
     });
-
 };
 
 exports.getImages = function(req, res) {
@@ -33,5 +31,32 @@ exports.getImages = function(req, res) {
         }
 
         res.json(images);
+    });
+};
+
+exports.getImage = function(req, res) {
+    let imgId = req.params.id;
+
+    Image.findById(imgId, (err, image) => {
+        if (err) {
+            return res.sendStatus(400);
+        }
+
+        res.setHeader('Content-Type', 'image/jpeg');
+        fs.createReadStream(path.join(UPLOAD_PATH, image.filename)).pipe(res);
+    });
+};
+
+exports.deleteImage = function(req, res) {
+    let imgId = req.params.id;
+
+    Image.findByIdAndRemove(imgId, (err, image) => {
+        if (err && image) {
+            return res.sendStatus(400);
+        }
+
+        del([path.join(UPLOAD_PATH, image.filename)]).then(deleted => {
+            res.sendStatus(200);
+        });
     });
 };
